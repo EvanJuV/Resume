cce989397516725cc61b7ff896b4c1e1f580469e
evanjuv <e.juav24@gmail.com>
2015-10-05 18:19:10 -0500

    Changes the structure of the file tree, adds gulp to run locally, adds JQuery, and finishes the progress bars

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+// watch files for changes and reload
+gulp.task('serve', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  });
+
+  gulp.watch(['*.html', 'stylesheets/**/*.css', 'js/**/*.js'], {cwd: 'app'}, reload);
+});
